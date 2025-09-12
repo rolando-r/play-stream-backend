196f5eb6eded0c3449963788923a8bfc7f56773b
rolando-r <131362377+rolando-r@users.noreply.github.com>
2025-09-12 13:06:22 -0500

    feat(migrations): add contents, movies, series and episodes tables

---
+++ src/database/migrations/2025_09_12_175955_create_episodes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('series_id')->constrained('series')->onDelete('cascade');
+            $table->integer('season_number');
+            $table->integer('episode_number');
+            $table->string('title');
+            $table->integer('duration')->nullable(); // in minutes
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('episodes');
+    }
+};
